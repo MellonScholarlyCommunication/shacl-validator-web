3b31716d8daf6d1e4f92e2732f25005ff9ecc170
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-12-03 09:16:15 +0100

    Bla

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,32 @@
 // place files you want to import through the `$lib` alias in this folder.
+import factory from 'rdf-ext';
+import rdfParser from 'rdf-parse';
+import rdfSerializer from 'rdf-serialize';
+import stringifyStream from 'stream-to-string';
+import SHACLValidator from 'rdf-validate-shacl';
+import Streamify from 'streamify-string';
+
+async function loadDataset (dataStr,type) {
+    const sm = Streamify(dataStr);
+    return factory.dataset().import(
+      rdfParser.parse(sm, { contentType: type })
+    );
+}
+
+export async function validateString(dataStr) {
+    try {
+        const res = await fetch('eventnotifications.ttl');
+        const SHAPE_FILE = await res.text();
+        const shapes = await loadDataset(SHAPE_FILE,'text/turtle');
+        const data = await loadDataset(dataStr,'application/ld+json');
+        const validator = new SHACLValidator(shapes, { factory });
+        const report = await validator.validate(data);
+        const quadStream = report.dataset.toStream();
+        const textStream = rdfSerializer.serialize(quadStream, { contentType: 'text/turtle' });
+        return await stringifyStream(textStream);
+    }
+    catch (e) {
+        console.log(e);
+        return `failed: ${e}`;
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { validateString } from '$lib/index.js';
+
+    let result = '';
+    let value = '';
+
+    async function validate() {
+        result = await validateString(value);
+    }
+</script>
+
+<h1>Event Notification Validator</h1>
+
+<p>
+Paste your notification here:<br/>
+<textarea id="validation-body" bind:value={value}></textarea>
+<br/>
+<button on:click={validate}>Validate</button>
+
+{#if result}
+<h2 id="result-header">Validation Result</h2>
+<pre id="result-body">
+{result}
+</pre>
+{/if}
+</p>
+
+<style>
+    textarea {
+        width: 800px;
+        height: 400px;
+    }
+</style>
